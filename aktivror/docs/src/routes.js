6bd2e557584c920e55fa75d1e3b99b33cea02dd9
DESKTOP-FHRENU6\Alexander <Alexan2405@gmail.com>
2019-07-16 16:05:01 +0200

    no message

--- aktivror/docs/src/routes.js
+++
@@ -1,7 +0,0 @@
-import Installation from "./pages/Installation.vue";
-import MainPage from "./pages/MainPage.vue";
-
-export default [
-  { path: "/aktivror", component: MainPage },
-  { path: "*", redirect: "/aktivror" }
-];
--- aktivror/src/index.js
+++
@@ -1,4 +0,0 @@
-import './../node_modules/dropzone/dist/dropzone.css'
-import vueDropzone from './components/vue-dropzone.vue'
-
-export default vueDropzone
